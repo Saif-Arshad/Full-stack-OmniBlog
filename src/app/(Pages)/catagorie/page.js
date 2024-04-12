b8cdbdf84689a97a1c46cd509a1cd7a6e4246616
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-12 22:03:22 +0500

    build bug resolve

--- src/app/(Pages)/catagorie/page.js
+++ src/app/(Pages)/catagorie/page.js
@@ -1,5 +1,5 @@
 // /* eslint-disable react-hooks/rules-of-hooks */
-// 'use client'
+'use client'
 // import React, { useEffect,useState } from 'react'
 // import { useSearchParams } from 'next/navigation'
 // import { Suspense } from 'react'
